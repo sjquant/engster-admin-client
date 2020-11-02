4a3d06a0eac4c45746f98c2b251531e1abb3d594
sjquant <seonujang92@gmail.com>
2020-11-03 01:04:28 +0900

    Work on login page ui

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,7 +1,33 @@
 <template>
-  <h1>Login</h1>
+  <q-layout>
+    <q-page-container>
+      <q-page class="flex justify-center full-width items-center">
+        <div class="login-form-container q-mx-sm">
+          <q-input v-model="email" label="Email" class="q-mb-lg" />
+          <q-input v-model="password" label="Password" class="q-mb-lg" />
+          <div class="flex justify-end">
+            <q-btn color="primary">로그인</q-btn>
+          </div>
+        </div>
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+};
 </script>
+
+<style lang="scss" scoped>
+.login-form-container {
+  width: 100%;
+  max-width: 400px;
+}
+</style>

18ebf5ac01ac65543dc4145b2d170e7f3e034c65
sjquant <seonujang92@gmail.com>
2021-03-27 18:20:33 +0900

    Working on table ui

---
+++ src/components/TranslationReviewStatusTabs.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="tab-container">
+    <q-tabs dense v-model="tab" align="justify" class="text-grey-7">
+      <q-tab :ripple="false" name="pending" label="대기중" />
+      <q-tab :ripple="false" name="processed" label="처리내역" />
+    </q-tabs>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      tab: "pending",
+    };
+  },
+};
+</script>
+<style scoped lang="scss">
+.tab-container {
+  width: 200px;
+}
+</style>
--- src/pages/TranslationReviews.vue
+++ src/pages/TranslationReviews.vue
@@ -1,3 +1,26 @@
 <template>
-  <q-page></q-page>
+  <q-page>
+    <div class="q-pa-lg">
+      <div class="q-pb-lg full-width flex justify-end">
+        <StatusTabs />
+      </div>
+      <PendingTable />
+    </div>
+  </q-page>
 </template>
+<script>
+import StatusTabs from "../components/TranslationReviewStatusTabs.vue";
+import PendingTable from "../components/TranslationReviewPendingTable.vue";
+
+export default {
+  components: {
+    StatusTabs,
+    PendingTable,
+  },
+};
+</script>
+<style lang="scss">
+.translation-review-table {
+  max-height: calc(100vh - 160px);
+}
+</style>
